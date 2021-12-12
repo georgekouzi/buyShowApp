ad3e190f5316ae1c47cbf810607d5867917f710c
george <georgekouzi@gmail.com>
2021-12-12 10:12:31 +0200

    Activity category_sellers

---
+++ app/src/main/java/com/example/buyshow/SellersCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.buyshow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellersCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sellers_category);
+    }
+}
