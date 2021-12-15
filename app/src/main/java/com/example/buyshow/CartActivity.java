29004db87ac8c3c2bcc35006d7ea31cc9637ec08
dolevsaadia <57878915+dolevsaadia@users.noreply.github.com>
2021-12-15 20:08:38 +0200

    all cart page with design

--- app/src/main/java/com/example/buyshow/CartActivity.java
+++ app/src/main/java/com/example/buyshow/CartActivity.java
@@ -1,14 +1,28 @@
 package com.example.buyshow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CartActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private Button nextbtn;
+    private TextView totalamounttxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+        recyclerView = findViewById(R.id.cart_list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        nextbtn =(Button) findViewById(R.id.next_proces_btn);
+        totalamounttxt = (TextView) findViewById(R.id.total_price);
     }
 }
